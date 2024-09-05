24469a2e704d4773a2cb7edee73536f429b32d21
peterberekvolgyi <berekvolgyipeter@gmail.com>
2024-09-05 19:24:34 +0200

    fix format

--- script/DeploySimpleStorage.s.sol
+++ script/DeploySimpleStorage.s.sol
@@ -6,7 +6,7 @@ import {Script} from "forge-std/Script.sol";
 import {SimpleStorage} from "../src/SimpleStorage.sol";
 
 contract DeploySimpleStorage is Script {
-    function run() external returns(SimpleStorage) {
+    function run() external returns (SimpleStorage) {
         // vm is a special keyword in foundry
         // see more at: https://book.getfoundry.sh/cheatcodes/
         vm.startBroadcast();
